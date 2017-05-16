c7823a49d3d57a2c38ec2c8cdf4bae269bd47917
Thiha Aung <thihaaung@creative-webstudio.comhtarwara.6245@gmail.com>
2017-05-16 21:34:10 +0630

    Old Changes updated to repo

--- ACTabScrollView/ContentViewController.swift
+++ ACTabScrollView/ContentViewController.swift
@@ -56,31 +56,6 @@ class ContentViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         return cell
     }
-    
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 48
-    }
-    
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let view = UIView()
-        view.backgroundColor = UIColor(red: 61.0 / 255, green: 66.0 / 255, blue: 77.0 / 255, alpha: 1.0)
-        
-        let label = UILabel()
-        label.text = "Today"
-        label.textColor = UIColor.white
-        if #available(iOS 8.2, *) {
-            label.font = UIFont.systemFont(ofSize: 17, weight: UIFontWeightThin)
-        } else {
-            label.font = UIFont.systemFont(ofSize: 17)
-        }
-        label.sizeToFit()
-        label.frame.origin = CGPoint(x: 18, y: 13)
-        
-        view.addSubview(label)
-        
-        return view
-    }
-
 }
 
 class ContentTableViewCell: UITableViewCell {
